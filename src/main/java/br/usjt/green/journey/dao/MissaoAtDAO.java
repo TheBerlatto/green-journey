90d717ee9480f13f7c8923b913d077d929c7ecef
vitor nunes <kira18nunes19@gmail.com>
2023-05-27 10:45:28 -0300

    modificações no metodo de missaoAtDAO
    modificações no metodo de inserir

--- src/main/java/br/usjt/green/journey/dao/MissaoAtDAO.java
+++ src/main/java/br/usjt/green/journey/dao/MissaoAtDAO.java
@@ -20,19 +20,67 @@ public class MissaoAtDAO {
        
         String sqlUsuario = "SELECT id FROM tb_usuario WHERE username = ?";
         String sqlMissao = "SELECT id FROM tb_missao";
-    try (Connection conn = ConnectionFactory.obtemConexao();
-            /*modificar para que o codigo pegue o id do usuario que se cadastrou*/
-             PreparedStatement psUsuario = conn.prepareStatement(sqlUsuario);
-             PreparedStatement psMissao = conn.prepareStatement(sqlMissao);
-             ResultSet rsUsuario = psUsuario.executeQuery();
-             ResultSet rsMissao = psMissao.executeQuery()) {
-            ps.setInt(1, idUsuario);
-            ps.setInt(2, idMissao);
-            ps.executeUpdate();
+        
+        try (Connection conn = ConnectionFactory.obtemConexao();
+             PreparedStatement stmtUsuario = conn.prepareStatement(sqlUsuario);
+             PreparedStatement stmtMissao = conn.prepareStatement(sqlMissao)) {
+            
+            // Obtém o ID do usuário pelo nome
+            stmtUsuario.setString(1, username);
+            ResultSet rsUsuario = stmtUsuario.executeQuery();
+            if (rsUsuario.next()) {
+                int idUsuario = rsUsuario.getInt("id");
+                
+                // Obtém todos os IDs das missões
+                ResultSet rsMissao = stmtMissao.executeQuery();
+                while (rsMissao.next()) {
+                    int idMissao = rsMissao.getInt("id");
+                    
+                    // Verifica se já existe a associação entre usuário e missão
+                    if (!existeAssociacao(idUsuario, idMissao, conn) && !existeMissao(idMissao, conn)) {
+                        // Insere a associação entre usuário e missão
+                        String sqlInsert = "INSERT INTO tb_missao_usuario (id_usuario, id_missao) VALUES (?, ?)";
+                        try (PreparedStatement stmtInsert = conn.prepareStatement(sqlInsert)) {
+                            stmtInsert.setInt(1, idUsuario);
+                            stmtInsert.setInt(2, idMissao);
+                            stmtInsert.executeUpdate();
+                        }
+                    }
+                }
+            }
+            
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
     
+    }
+    private boolean existeAssociacao(int idUsuario, int idMissao, Connection conn) throws SQLException{
+    /*metodo que verifica se ja existe uma associação do usuario com a missão*/
+     String sql = "SELECT COUNT(*) FROM tb_missaoAt WHERE id_usuario = ? AND id_missao = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, idUsuario);
+            stmt.setInt(2, idMissao);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                int count = rs.getInt(1);
+                return count > 0;
+            }
+        }
+        return false;
+    
+    }
+    private boolean existeMissao(int idMissao, Connection conn) throws SQLException{
+    /*verifica se a tabela ja possui a missão*/
+    String sql = "SELECT COUNT(*) FROM tb_missaoAt WHERE id = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, idMissao);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                int count = rs.getInt(1);
+                return count > 0;
+            }
+        }
+        return false;
     }
     public void alterarEstado(){
     /*altera estado da missão de não finalizada para finalizxada*/
