c48026e8adfbfae6a7d6cc16cee11285ac62119e
vitor nunes <kira18nunes19@gmail.com>
2023-05-27 08:43:08 -0300

    criação dos metodos na classe MissaoAtDAO
    foi criado o metodo inserir missão e alterar estado da missão

--- src/main/java/br/usjt/green/journey/dao/MissaoAtDAO.java
+++ src/main/java/br/usjt/green/journey/dao/MissaoAtDAO.java
@@ -4,10 +4,39 @@
  */
 package br.usjt.green.journey.dao;
 
+import br.usjt.green.journey.connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author usuario
  */
 public class MissaoAtDAO {
+    
+    public void inserirMissaoAt(String username) throws Exception{
+       
+        String sqlUsuario = "SELECT id FROM tb_usuario WHERE username = ?";
+        String sqlMissao = "SELECT id FROM tb_missao";
+    try (Connection conn = ConnectionFactory.obtemConexao();
+            /*modificar para que o codigo pegue o id do usuario que se cadastrou*/
+             PreparedStatement psUsuario = conn.prepareStatement(sqlUsuario);
+             PreparedStatement psMissao = conn.prepareStatement(sqlMissao);
+             ResultSet rsUsuario = psUsuario.executeQuery();
+             ResultSet rsMissao = psMissao.executeQuery()) {
+            ps.setInt(1, idUsuario);
+            ps.setInt(2, idMissao);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    
+    }
+    public void alterarEstado(){
+    /*altera estado da missão de não finalizada para finalizxada*/
+    
+    }
     
 }
