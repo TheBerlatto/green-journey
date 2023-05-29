a6de7594ee10cfab21e68a9eab81fd6caf4e24e5
vitor nunes <kira18nunes19@gmail.com>
2023-05-29 14:19:43 -0300

    teste

--- src/main/java/GreenJourney.java
+++ src/main/java/GreenJourney.java
@@ -9,6 +9,6 @@
 public class GreenJourney {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World olaaaaaaaa!");
     }
 }
